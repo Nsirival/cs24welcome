e4570bf735b21d7a38dfd6564bb1af49b01834d3
Nate Sirivallop <nsirival1@gmail.com>
2024-04-10 14:45:28 -0700

    started fibvec

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -1,7 +1,40 @@
 #include "FibVec.h"
-
+#include <vector>
 // This provides exception types:
 #include <stdexcept>
 
-
 // FibVec Function Implementations
+FibVec::FibVec(){
+    this-> cap = 1;
+    this-> cnt = 0;
+    f = new int[cap];
+}
+FibVec::~FibVec(){
+    delete[] f;
+}
+size_t FibVec::capacity() const{
+    return cap;
+}
+size_t FibVec::count() const{
+    return cnt;
+}
+void FibVec::insert(size_t value, size_t index){
+    
+    cnt ++;
+}
+size_t FibVec::lookup(size_t index) const{
+    if(index > cnt){
+        throw std::out_of_range("Index out of range");
+    }
+    return f[index];
+}
+void FibVec::pop(){
+    
+}
+void FibVec::push(size_t value){
+
+}
+void FibVec::remove(size_t index){
+    cnt --;
+}
+
--- labs/fibvec/FibVec.h
+++ labs/fibvec/FibVec.h
@@ -4,16 +4,27 @@
 // This provides the size_t type:
 #include <cstddef>
 
+
 class FibVec {
   // Member Variables
-
+  int *f;
+  size_t cnt;
+  size_t cap;
   // Helper Functions
-
+  
 public:
   // Constructor and Destructor
-
+  FibVec();
+  ~FibVec();
+  
   // Member Functions
-
+  size_t capacity()const;
+  size_t count() const;
+  void insert(size_t value, size_t index);
+  size_t lookup(size_t index) const;
+  void pop();
+  void push(size_t value);
+  void remove(size_t index);
 };
 
 #endif
