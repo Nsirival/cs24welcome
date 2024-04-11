351b9364cf90fae244546fb68e0818a66c811c16
Nsirival <55929427+Nsirival@users.noreply.github.com>
2024-04-10 19:09:02 -0700

    Update FibVec.cpp

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -39,7 +39,6 @@ void FibVec::insert(int value, size_t index){
         for(size_t i = 0; i < cnt; i++){
             newf[i] = f[i];
         }
-        delete [] f;
     
         f = newf;
         cap = fib;
