aa968739c140e56151f7318b7c7eea3aa20b0546
Nsirival <55929427+Nsirival@users.noreply.github.com>
2024-04-10 20:42:13 -0700

    Update FibVec.cpp

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -38,7 +38,7 @@ size_t FibVec::count() const{
     return cnt;
 }
 void FibVec::insert(int value, size_t index){
-    if(index > cnt){
+    if(index >= cnt){
         throw std::out_of_range("Index out of range");
     }
 
