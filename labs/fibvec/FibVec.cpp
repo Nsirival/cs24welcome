eb74c55ee726adf08e70fc56f74cfcb6c6a17a4b
Nsirival <55929427+Nsirival@users.noreply.github.com>
2024-04-15 02:29:49 -0700

    Update FibVec.cpp

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -100,9 +100,9 @@ size_t FibVec::remove(size_t index){
     } else {
     
         if(cnt < FibVec::fibnum1((int)(FibVec::number1(cnt+2)) - (int)(FibVec::number1(cnt)))){
-            cap = FibVec::fibnum1((int)(FibVec::number1(cnt))+ 3);
+            cap = FibVec::fibnum1((int)(FibVec::number1(cnt))+ 2);
         } else {
-            cap = FibVec::fibnum1((int)(FibVec::number1(cnt))+ 1);
+            cap = FibVec::fibnum1((int)(FibVec::number1(cnt))+ 2);
         }
     
         size_t ret = (size_t)f[index];
