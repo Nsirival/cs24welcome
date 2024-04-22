bed2a876c94c5f0db2e60807308a9565d6e18200
Nate Sirivallop <nsirival1@gmail.com>
2024-04-22 01:34:38 -0700

    add da comment

--- labs/tictac/Move.cpp
+++ labs/tictac/Move.cpp
@@ -13,76 +13,116 @@
 //   char player;
 //   int  row;
 //   int  column;
-//1 X B2
+// 1 X B2
 // Space for implementing Move functions.
-Move::Move(const std::string& input){
+Move::Move(const std::string &input)
+{
     int i = 1;
-    if(input.length() >= 6){
-        
-        if(isdigit(input[0]) && isspace(input[1])){
-            if(((int)input[0] -48<= 9) &&((int)input[0] -48>= 1)){
-                number = (int)input[0] -48;
-            } else {
-            std::cout << "Parse error." << std::endl;
-            exit(1);
-            } 
-        } else {
-            std::cout << "Parse error." << std::endl;
-            exit(1);
-        } 
-        while((isspace(input[i])) && (i < (int)input.length()-1)){
-            i++;
+    if (isdigit(input[0]) && isspace(input[1]))
+    {
+        if (((int)input[0] - 48 <= 9) && ((int)input[0] - 48 >= 1))
+        {
+            number = (int)input[0] - 48;
         }
-        if((input[i] == 'X' || input[i] == 'O') || (input[i] == 'x' || input[i] == 'o')){
-            player = (int)input[i];
-        } else {
-            std::cout << "Parse error." << std::endl;
-            exit(1);
-        } 
-        if(i < (int)input.length()){
-            i++;
-        } else {
+        else
+        {
             std::cout << "Parse error." << std::endl;
             exit(1);
         }
-        while((isspace(input[i])) && (i < (int)input.length()-1)){
-            i++;
-        }
-
-        if(input[i] == 'A' || input[i] == 'a'){
-            row = 1;
-        } else if(input[i] == 'B' || input[i] == 'b'){
-            row = 2;
-        } else if(input[i] == 'C' || input[i] == 'c'){
-            row = 3;
-        } else {
-            std::cout << "Parse error." << std::endl;
-            exit(1);
-        }
-        if(i < (int)input.length()){
-            i++;
-        } else {
-            std::cout << "Parse error." << std::endl;
-            exit(1);
-        }
-        if((input[i] == '1') || (input[i] == '2') || (input[i] == '3')){
-            column = (int)input[i]-48;
-        }else {
-            std::cout << "Parse error." << std::endl;
-            exit(1);
-        }
-
-    }else {
-        exit(1);
+    }
+    else
+    {
         std::cout << "Parse error." << std::endl;
+        exit(1);
+    }
+    while ((isspace(input[i])) && (i < (int)input.length() - 1))
+    {
+        i++;
+    }
+    if ((input[i] == 'X' || input[i] == 'O') || (input[i] == 'x' || input[i] == 'o'))
+    {
+        player = (int)input[i];
+    }
+    else
+    {
+        std::cout << "Parse error." << std::endl;
+        exit(1);
+    }
+    if (i < (int)input.length())
+    {
+        i++;
+    }
+    else
+    {
+        std::cout << "Parse error." << std::endl;
+        exit(1);
+    }
+    while ((isspace(input[i])) && (i < (int)input.length() - 1))
+    {
+        i++;
     }
 
-    
-    
+    if (input[i] == 'A' || input[i] == 'a')
+    {
+        row = 1;
+    }
+    else if (input[i] == 'B' || input[i] == 'b')
+    {
+        row = 2;
+    }
+    else if (input[i] == 'C' || input[i] == 'c')
+    {
+        row = 3;
+    }
+    else
+    {
+        std::cout << "Parse error." << std::endl;
+        exit(1);
+    }
+    if (i < (int)input.length())
+    {
+        i++;
+    }
+    else
+    {
+        std::cout << "Parse error." << std::endl;
+        exit(1);
+    }
+    if ((input[i] == '1') || (input[i] == '2') || (input[i] == '3'))
+    {
+        column = (int)input[i] - 48;
+    }
+    else
+    {
+        std::cout << "Parse error." << std::endl;
+        exit(1);
+    }
+    if (i < (int)input.length() - 1)
+    {
+        i++;
+        if (!isspace(input[i]))
+        {
+            std::cout << "Parse error." << std::endl;
+            exit(1);
+        }
+        else
+        {
+            if (input[i] != '#')
+            {
+                std::cout << "Parse error." << std::endl;
+                exit(1);
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Parse error." << std::endl;
+        exit(1);
+    }
 };
 
-
-std::string Move::to_string() const{
+std::string Move::to_string() const
+{
     // std::string str = "";
     // str += (std::string)number;
     // str += " ";
@@ -92,5 +132,5 @@ std::string Move::to_string() const{
     // if (row == 2){str += "B";}
     // if (row == 3){str += "C";}
     // str += (std::string)column;
-     return "h";
+    return "h";
 }
