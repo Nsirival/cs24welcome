c63e667a557756585b5675ad3aa3508818060aa8
Nate Sirivallop <nsirival1@gmail.com>
2024-04-22 10:44:45 -0700

    s

--- labs/tictac/game-check.cpp
+++ labs/tictac/game-check.cpp
@@ -19,33 +19,24 @@ int main(int argc, char **argv)
     }
 
     Board board;
-
+    Move move;
     std::string line;
+    
     while (getline(std::cin, line))
     {
         board.checkWin();
-        Move move(line);
+        Move move (line);
         if (board.getgame() == false)
         {
-            try
-            {
+            
                 board.add_move(move.getmovenum(), move.getplayer(), move.getrow(), move.getcolumn());
-            }
-            catch (const InvalidMove &e)
-            {
-                if (verbose)
-                {
-                    std::cout << "Invalid move." << e.what() << '\n';
-                }
-                else
-                {
-                    std::cout << "Invalid move.\n";
-                }
-            }
+            
+            
         }
         else
         {
             board.getstatus();
+            exit(0);
         }
     }
 }
