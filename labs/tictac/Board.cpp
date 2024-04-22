fe18722ce596a3627c0ea1b170767d46373f58ee
Nate Sirivallop <nsirival1@gmail.com>
2024-04-22 10:04:27 -0700

    s

--- labs/tictac/Board.cpp
+++ labs/tictac/Board.cpp
@@ -2,8 +2,10 @@
 #include "Board.h"
 #include <iostream>
 // Space for implementing Board functions.
-Board::Board(){
-    for(int i = 0; i < (int)(sizeof(brd)/(int)sizeof(brd[0])); i++){
+Board::Board()
+{
+    for (int i = 0; i < (int)(sizeof(brd) / (int)sizeof(brd[0])); i++)
+    {
         brd[i] = '0';
     }
     movecount = 0;
@@ -11,95 +13,130 @@ Board::Board(){
     lastplayed = '0';
 }
 
-char Board::checkWin(){
-    if((brd[0] == brd[1]) & (brd [1] == brd[2])){
-        return (char)brd[1];gameover = true;
+char Board::checkWin()
+{
+    if (((brd[0] == brd[1]) && (brd[1] == brd[2])) && brd[1] != '0')
+    {
+        return (char)brd[1];
+        gameover = true;
     }
-    if((brd[3] == brd[4]) & (brd [4] == brd[5])){
-        return (char)brd[4];gameover = true;
+    if (((brd[3] == brd[4]) && (brd[4] == brd[5])) && brd[4] != '0')
+    {
+        return (char)brd[4];
+        gameover = true;
     }
-    if((brd[6] == brd[7]) & (brd [7] == brd[8])){
-        return (char)brd[7];gameover = true;
+    if (((brd[6] == brd[7]) && (brd[7] == brd[8])) && brd[7] != '0')
+    {
+        return (char)brd[7];
+        gameover = true;
     }
-    if((brd[0] == brd[3]) & (brd [3] == brd[6])){
-        return (char)brd[3];gameover = true;
+    if (((brd[0] == brd[3]) && (brd[3] == brd[6])) && brd[3] != '0')
+    {
+        return (char)brd[3];
+        gameover = true;
     }
-    if((brd[1] == brd[4]) & (brd [4] == brd[7])){
-        return (char)brd[4];gameover = true;
+    if (((brd[1] == brd[4]) && (brd[4] == brd[7])) && brd[4] != '0')
+    {
+        return (char)brd[4];
+        gameover = true;
     }
-    if((brd[2] == brd[5]) & (brd [5] == brd[8])){
-        return (char)brd[5];gameover = true;
+    if (((brd[2] == brd[5]) && (brd[5] == brd[8])) && brd[5] != '0')
+    {
+        return (char)brd[5];
+        gameover = true;
     }
-    if((brd[0] == brd[4]) & (brd [4] == brd[8])){
-        return (char)brd[4];gameover = true;
+    if (((brd[0] == brd[4]) && (brd[4] == brd[8])) && brd[4] != '0')
+    {
+        return (char)brd[4];
+        gameover = true;
     }
-    if((brd[2] == brd[4]) & (brd [4] == brd[6])){
+    if (((brd[2] == brd[4]) && (brd[4] == brd[6])) && brd[4] != '0') 
+    {
         return (char)brd[4];
         gameover = true;
     }
     return '0';
 }
 
-void Board::add_move(int movenum,char player, int row, int column){
-    movecount ++;
-    if(movenum != movecount){
+void Board::add_move(int movenum, char player, int row, int column)
+{
+    movecount++;
+    if (movenum != movecount)
+    {
         InvalidMove hi("Invalid move.");
         throw hi;
         exit(2);
     }
-    if(lastplayed == player){
+    if (lastplayed == player)
+    {
         InvalidMove hi("Invalid move.");
         throw hi;
         exit(2);
     }
-    if(gameover == true){
+    if (gameover == true)
+    {
         InvalidMove hi("Invalid move.");
         throw hi;
         exit(2);
     }
-    if(brd[column-1+(row-1)*3] != 'X' && brd[column-1+(row-1)*3] != 'O'){
-        if(row == 1){
-            brd[column-1] = player;
-        } else if(row == 2){
-            brd[column-1 + 3] = player;
-        } else if(row == 3){
-            brd[column-1 + 6] = player;
+    if (brd[column - 1 + (row - 1) * 3] != 'X' && brd[column - 1 + (row - 1) * 3] != 'O')
+    {
+        if (row == 1)
+        {
+            brd[column - 1] = player;
         }
-        
-    } else {
+        else if (row == 2)
+        {
+            brd[column - 1 + 3] = player;
+        }
+        else if (row == 3)
+        {
+            brd[column - 1 + 6] = player;
+        }
+    }
+    else
+    {
         InvalidMove hi("Invalid move.");
         throw hi;
         exit(2);
     }
     lastplayed = player;
     movenumber = movenum;
-    
 }
 
-std::string Board::getstatus(){
+std::string Board::getstatus()
+{
     char h = static_cast<char>(Board::checkWin());
-    if( h == 'X'){
+    if (h == 'X')
+    {
         std::cout << "Game over: X wins." << std::endl;
         gameover = true;
-    } 
-    if(h == 'O'){
+    }
+    if (h == 'O')
+    {
         std::cout << "Game over: O wins." << std::endl;
         gameover = true;
-    } 
-    if((h == '0') && movecount == 9){
+    }
+    if ((h == '0') && movecount == 9)
+    {
         std::cout << "Game over: Draw." << std::endl;
         gameover = true;
     }
-    if(movecount == 0){
+    if (movecount == 0)
+    {
         return "Game in progress: New game.";
-    } 
-    if(movecount %2 == 0){
+    }
+    if (movecount % 2 == 0)
+    {
         return "Game in progress: X's turn.";
-    } else {
+    }
+    else
+    {
         return "Game in progress: O's turn.";
     }
 }
 
-bool Board::getgame(){
+bool Board::getgame()
+{
     return gameover;
 }
