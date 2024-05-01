bcae94c1554efb1e933a9129c4d54c357ebe9801
Nate Sirivallop <nsirival1@gmail.com>
2024-05-01 01:25:18 -0700

    s

--- labs/rotate/Tree.h
+++ labs/rotate/Tree.h
@@ -3,24 +3,30 @@
 
 #include "Node.h"
 
-class Tree {
-  // Member Variables
-
+class Tree
+{
+  size_t num;
+  Node *hi;
+  size_t nodecount = 0;
+  size_t randomnum = -1;
   // Private Helper Functions
+  size_t recursivecount(Node *start);
+  void recursiveclear(Node *start);
+  size_t recursivefind(Node *start, std::string value);
 
 public:
   Tree();
   ~Tree();
 
   // Public Member Functions
-  void        clear();
-  size_t      count() const;
-  bool        contains(const std::string& s) const;
-  size_t      find(const std::string& s) const;
-  void        insert(const std::string& s);
+  void clear();
+  size_t count() const;
+  bool contains(const std::string &s);
+  size_t find(const std::string &s);
+  void insert(const std::string &s);
   std::string lookup(size_t index) const;
-  void        print() const;
-  void        remove(size_t index);
+  void print() const;
+  void remove(size_t index);
 };
 
 #endif
