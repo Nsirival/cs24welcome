91f8dd4442a9a39ab3e42276874a988b62c1ffa6
Nate Sirivallop <nsirival1@gmail.com>
2024-05-07 14:34:30 -0700

    s

--- labs/polish/AST.cpp
+++ labs/polish/AST.cpp
@@ -3,7 +3,6 @@
 #include "Stack.h"
 #include <sstream>
 
-
 AST *AST::parse(const std::string &expression)
 {
     std::string token;
@@ -12,7 +11,8 @@ AST *AST::parse(const std::string &expression)
 
     while (stream >> token)
     {
-        if(token == "+" || token == "-" || token == "*" || token == "/" || token == "~" || token == "%"){
+        if (token == "+" || token == "-" || token == "*" || token == "/" || token == "%")
+        {
             if (stack.root == nullptr)
             {
                 throw std::runtime_error("Not enough operands");
@@ -23,20 +23,38 @@ AST *AST::parse(const std::string &expression)
                 throw std::runtime_error("Not enough operands");
             }
             AST *left = stack.pop();
-            if (token == "+"){
+            if (token == "+")
+            {
                 stack.push(new add(left, right));
-            } else if (token == "-"){
-                stack.push(new sub(left, right));
-            } else if (token == "*"){
-                stack.push(new mult(left, right));
-            } else if (token == "/"){
-                stack.push(new divd(left, right));
-            } else if (token == "%"){
-                stack.push(new mod(left, right));
-            } else if (token == "~"){
-                stack.push(new neg(left));
             }
-        } else if (isdigit(token[0]) || token[0] == '-' || token[0] == '+')
+            else if (token == "-")
+            {
+                stack.push(new sub(left, right));
+            }
+            else if (token == "*")
+            {
+                stack.push(new mult(left, right));
+            }
+            else if (token == "/")
+            {
+                stack.push(new divd(left, right));
+            }
+            else if (token == "%")
+            {
+                stack.push(new mod(left, right));
+            }
+        }
+        else if (token == "~")
+        {
+            if (stack.root == nullptr)
+            {
+                throw std::runtime_error("Not enough operands");
+            }
+            AST *right = stack.pop();
+            stack.push(new neg(right));
+        }
+
+        else if (isdigit(token[0]) || token[0] == '-' || token[0] == '+')
         {
             char *end;
             double result = strtod(token.c_str(), &end);
@@ -47,10 +65,10 @@ AST *AST::parse(const std::string &expression)
             stack.push(new nodes(result));
         }
     }
-    if (stack.root == nullptr){
+    if (stack.root == nullptr)
+    {
         throw std::runtime_error("No input");
     }
     AST *root = stack.pop();
     return root;
-    
 }
