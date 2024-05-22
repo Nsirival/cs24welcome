0e9331d7f2fe77ed3428f5a9860849e40605f124
Nate Sirivallop <nsirival1@gmail.com>
2024-05-22 07:41:14 -0700

    s

--- labs/counter/List.cpp
+++ labs/counter/List.cpp
@@ -1,3 +1,84 @@
 #include "List.h"
 
+Node::Node(const std::string &k, int d)
+{
+    key = k;
+    data = d;
+    prev = nullptr;
+    next = nullptr;
+}
 // List Member Functions
+
+List::List()
+{
+    head = nullptr;
+    tail = nullptr;
+}
+
+List::~List()
+{
+    Node *x = head;
+    while (x != nullptr)
+    {
+        Node *y = x->next;
+        delete x;
+        x = y;
+    }
+}
+
+void List::insert(const std::string &k, int d)
+{
+
+    Node *y = new Node(k, d);
+    if (tail == nullptr)
+    {
+        tail = y;
+        head = y;
+    }
+    tail->next = y;
+    y->prev = tail;
+    tail = y;
+}
+
+Node *List::find(const std::string &k)
+{
+    Node *x = head;
+    while (x != nullptr)
+    {
+        if (x->key == k)
+        {
+            return x;
+        }
+        x = x->next;
+    }
+    return nullptr;
+}
+void List::remove(Node *n)
+{
+    if (n == nullptr)
+    {
+        return;
+    }
+    if (n->prev)
+    {
+        n->prev->next = n->next;
+    }
+    else
+    {
+        head = n->next;
+    }
+
+    if (n->next)
+    {
+        n->next->prev = n->prev;
+    }
+    else
+    {
+        tail = n->prev;
+    }
+    delete n;
+}
+bool List::empty() const
+{
+    return head == nullptr;
+}
--- labs/counter/List.h
+++ labs/counter/List.h
@@ -1,6 +1,28 @@
 #ifndef LIST_H
 #define LIST_H
+#include <string>
 
-// TODO...
+struct Node {
+    std::string key;
+    int data;
+    Node* prev;
+    Node * next;
+
+    Node(const std::string& k, int d);
+};
+
+class List {
+    private:
+        Node* head;
+        Node* tail;
+    public:
+        List();
+        ~List();
+        void insert(const std::string& k, int d);
+        Node* find (const std::string& k);
+        void remove (Node* n);
+        bool empty() const;
+
+};
 
 #endif
