97699184b825b49134f3e0567420bf8c3b628c76
Nate Sirivallop <nsirival1@gmail.com>
2024-06-06 23:54:58 -0700

    s

--- labs/voxhop/VoxMap.h
+++ labs/voxhop/VoxMap.h
@@ -7,18 +7,21 @@
 #include "Point.h"
 #include "Route.h"
 
-class VoxMap {
-  // Member Variables
+class VoxMap
+{
+private:
+  int l = 0;
+  int w = 0;
+  int h = 0;
+  std::vector<std::vector<std::vector<bool>>> voxmap;
 
-
-  // Helper Functions
-
+  bool canstand(Point pt);
 
 public:
-  VoxMap(std::istream& stream);
-  ~VoxMap();
+  VoxMap(std::istream &stream);
 
   Route route(Point src, Point dst);
+  bool valid(const Point &pt);
 };
 
 #endif
